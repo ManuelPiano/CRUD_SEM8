bb411f8507a4a919a7cc1f561023dbf0bb351162
JavierLainez420 <javierlainez123@gmail.com>
2022-05-03 09:34:08 -0600

    MetodoGuardarRegistros

--- EstudianteCRUD/src/estudiantecrud/ConexionCRUD.java
+++ EstudianteCRUD/src/estudiantecrud/ConexionCRUD.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package estudiantecrud;
 
-/**
- *
- * @author ITCA
- */
+import java.sql.*;
+
 public class ConexionCRUD {
+    private final String servidor = "jdbc:mysql://127.0.0.1:3306/bd_estudiantecrud";
+    private final String usuario = "root";
+    private final String clave = "";
+    private final String driverConector = "com.mysql.jdbc.Driver";
+    private static Connection conexion;
     
+    public ConexionCRUD(){
+    try{
+    Class.forName(driverConector);
+    conexion = DriverManager.getConnection(servidor, usuario, clave);
+    }catch (ClassNotFoundException  | SQLException e){
+        System.out.println("Conexion fallida! Error! : " + e.getMessage());
+    }
+    }
+    public Connection getConnection(){
+    return conexion;
+    }
+    
+    public void guardarRegistros(String tabla, String camposTabla, String valoresCampos){
+    ConexionCRUD conectar = new ConexionCRUD();
+    Connection cone = conectar.getConnection();
+    try{
+    String sqlQueryStmt = "INSERT INTO " + tabla + " (" + camposTabla + ") VALUES (" + valoresCampos + ")";
+    Statement stmt;
+    stmt = cone.createStatement();
+    
+    }
+    }
 }
